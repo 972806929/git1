8f4c06f4824b39c7d53dd2c6ceb6876b675b14d9
于博 <972806929@qq.com>
2021-11-28 22:02:56 +0800

    学生

--- src/student.java
+++ src/student.java
@@ -1,9 +1,18 @@
 public class student {
     private String name;
     private String age;
+    private String adress;
 
     public student(String name, String age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
     }
 }
