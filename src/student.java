c95b140d0e0cb129d9a70890313b3c474be4c24c
于博 <972806929@qq.com>
2021-11-28 22:02:17 +0800

    学生

--- src/student.java
+++ src/student.java
@@ -1,4 +1,9 @@
 public class student {
     private String name;
     private String age;
+
+    public student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
 }
